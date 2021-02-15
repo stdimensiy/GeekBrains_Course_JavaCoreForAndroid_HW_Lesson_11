2a5ecfeff40ff6e99c638a9dc8be4c550560576d
StDimensiy <stdimensiy@yandex.ru>
2021-02-15 14:44:47 +0300

    Задание №1 - выполнено.

--- src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson11.java
+++ src/main/java/ru/geekbrains/JavaCoreForAndroid/Lesson11.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.JavaCoreForAndroid;
 
+import java.util.Arrays; // в данном примере использую чтобы быстро использовать встроенный метод toString
 /**
  * Сourse: java core for android
  * Faculty of Geek University Android Development
@@ -8,11 +9,30 @@ package ru.geekbrains.JavaCoreForAndroid;
  * Group 24.12.2020
  * <p>
  * HomeWork for lesson 11
- * Created 11.02.2021
+ * Created 14.02.2021
  * v1.0
  */
 public class Lesson11 {
     public static void main(String[] args) {
+        //Задание №1
+        Integer[] testArr1 = {1, 2, 3, 4};
+        System.out.print("Первоначальное состояние массива arr:");
+        System.out.println(Arrays.toString(testArr1));
+        SwapTwoArrayElements(testArr1, 1, 2);
+        System.out.print("Результат работы метода перестановки:");
+        System.out.println(Arrays.toString(testArr1));
+        String[] testArr2 = {"Первый", "Второй", "Третий", "Четвертый"};
+        System.out.print("Первоначальное состояние массива arr:");
+        System.out.println(Arrays.toString(testArr2));
+        SwapTwoArrayElements(testArr2, 1, 2);
+        System.out.print("Результат работы метода перестановки:");
+        System.out.println(Arrays.toString(testArr2));
+    }
 
+    // Метод работает с обобщенным типом данных, теперь ему все равно какой массив придет на вход.
+    public static <T> void SwapTwoArrayElements(T[] modifiableArray, int elemNum1, int elemNum2) {
+        T backupElement = modifiableArray[elemNum1];
+        modifiableArray[elemNum1] = modifiableArray[elemNum2];
+        modifiableArray[elemNum2] = backupElement;
     }
 }
